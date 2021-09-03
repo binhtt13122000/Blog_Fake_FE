d2f7ae9a8bb2ed61a734211984d173af516adea6
DanhVanPham <danhskipper18@gmail.com>
2021-09-03 10:09:46 +0700

    [DanhPV] fix error[DanhPV] fix layout[DanhPV] fix error

--- src/containers/App/index.tsx
+++
@@ -1,6 +0,0 @@
-// import ButtonBase from "../../components/Button/ButtonBase";
-import UserProfile from "../Users/UserProfile";
-
-export const App = () => {
-    return <UserProfile></UserProfile>;
-};
