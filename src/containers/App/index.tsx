9847c27f3c2ee69feb6d588ef7bfbd3f2c2d3b7c
DanhVanPham <danhskipper18@gmail.com>
2021-09-03 10:21:01 +0700

    [DanhPV] fix error

--- src/containers/App/index.tsx
+++ src/containers/App/index.tsx
@@ -1,22 +1,5 @@
-// import ButtonBase from "../../components/Button/ButtonBase";
-import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
-
-import AuthenticateProvider from "../../contexts/Authenticate/Login";
-import Login from "../Auth/Login";
-import Register from "../Auth/Register";
+// import UserProfile from "../Users/UserProfile";
 
 export const App = () => {
-    return (
-        <AuthenticateProvider>
-            <BrowserRouter>
-                <Switch>
-                    <Route exact path="/">
-                        <Redirect to="/login" />
-                    </Route>
-                    <Route path="/login" component={Login} />
-                    <Route path="/register" component={Register} />
-                </Switch>
-            </BrowserRouter>
-        </AuthenticateProvider>
-    );
+    return <div></div>;
 };
