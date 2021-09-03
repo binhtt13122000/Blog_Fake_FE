a29812b84323eb79e8cbf5d21f742de2b62de60a
DanhVanPham <danhskipper18@gmail.com>
2021-09-03 10:22:36 +0700

    [DanhPV] fix error

--- src/containers/App/index.tsx
+++ src/containers/App/index.tsx
@@ -1,6 +1,5 @@
-// import ButtonBase from "../../components/Button/ButtonBase";
-import ManageUser from "../Admin/ManageUser";
+// import UserProfile from "../Users/UserProfile";
 
 export const App = () => {
-    return <ManageUser></ManageUser>;
+    return <div></div>;
 };
