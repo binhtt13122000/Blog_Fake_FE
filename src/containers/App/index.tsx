eb6a7783afea24409786ab5a76cafca6916f6647
DanhVanPham <danhskipper18@gmail.com>
2021-09-05 17:11:30 +0700

    [DanhPV] create  add post page

--- src/containers/App/index.tsx
+++ src/containers/App/index.tsx
@@ -1,5 +1,5 @@
 // import ButtonBase from "../../components/Button/ButtonBase";
-import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import { CssBaseline } from "@material-ui/core";
 
@@ -7,6 +7,7 @@ import AuthenticateProvider from "../../contexts/Authenticate/Login";
 import ManageUser from "../Admin/ManageUser";
 import Login from "../Auth/Login";
 import Register from "../Auth/Register";
+import AddPost from "../Posts/AddPost";
 import UserProfile from "../Users/UserProfile";
 
 export const App = () => {
@@ -15,8 +16,8 @@ export const App = () => {
             <CssBaseline />
             <BrowserRouter>
                 <Switch>
-                    <Route exact path="/">
-                        <Redirect to="/login" />
+                    <Route exact path="/" component={AddPost}>
+                        {/* <Redirect to="/login" /> */}
                     </Route>
                     <Route path="/login" component={Login} />
                     <Route path="/register" component={Register} />
